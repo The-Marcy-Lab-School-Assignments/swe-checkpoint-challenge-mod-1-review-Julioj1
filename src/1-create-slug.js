63aad9a88078609f325b4808bcf64a80f4be3630
Julioj1 <dbjulioj@gmail.com>
2025-12-11 18:45:21 -0500

    Completed 1-create-slug

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,7 +1,14 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
 const createSlug = (title) => {
-
+  const bannedCharacters = ['!', '#', '?'];
+   for (let i = 0; i < bannedCharacters.length; i++) {
+    if (title.includes(bannedCharacters[i])) {
+      return null;
+    }
+  }
+  const slug = title.toLowerCase().replaceAll(' ', "-");
+  return slug;
 };
 
 module.exports = {
